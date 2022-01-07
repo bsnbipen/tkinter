3b421becd035709b4149ab19e070f28c8588c3e8
kale.kaji <bsnbipendra@gmail.com>
2022-01-07 16:55:19 -0500

    Friday Commit

--- learning example/trial_1.py
+++ learning example/trial_1.py
@@ -1,8 +1,14 @@
 from tkinter import *
+from tkinter.messagebox import *
 
-win=Frame()
-win.pack(expand=YES,fill=BOTH)
-Button(win,text="Quit").pack(side=LEFT, anchor=N)
-Label(win,text="I am here second").pack(side=TOP)
-Button(win,text="Press Me").pack(side=BOTTOM)
-win.mainloop()
+class Quitter(Frame):
+    def __init__(self, parent=None):
+        Frame.__init__(self,parent)
+        self.pack()
+        widget=Button(self, text='Quit',command=self.quit)
+        widget.pack(side=LEFT,expand=YES,fill=BOTH)
+    def quit(self):
+        ans=askokcancel('Verify Exit','Really Quit')
+        if ans: Frame.quit(self)
+
+if __name__=='__main__':Quitter().mainloop()
